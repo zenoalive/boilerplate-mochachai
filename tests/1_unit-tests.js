4f35291dba768155e151efc2f08028268641dde4
Zenoalive <ashushahi22229@gmail.com>
2024-02-06 18:26:17 +0530

    How JavaScript Assertions Work

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,26 +5,26 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNotNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+      assert.isNotNull(null, 'null is not undefined');
+      assert.isNotNull(undefined, 'undefined IS undefined');
+      assert.isNotNull('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isNotNull(null, 'null is falsey');
+      assert.isNotNull("I'm truthy", 'A string is truthy');
+      assert.isNotNull(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isNotNull(true, 'true is true');
+      assert.isNotNull(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotNull({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
